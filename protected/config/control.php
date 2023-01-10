5840e0a302eea4c89accd7e274b2954ff52537c4
shenchao <shenchao@lbsgroup.com.cn>
2023-01-10 11:24:07 +0800

    清除雜項權限

--- protected/config/control.php
+++ protected/config/control.php
@@ -1,25 +1,4 @@
 <?php
 
 return array(
-	'Staff Word'=>array('access'=>'ZR01',),//选择合同文档下载
-	'Contract Pay'=>array('access'=>'ZR02',),//合同工資
-	'All Overtime work List'=>array('access'=>'ZR03',),//所有加班列表
-	'All Ask leave List'=>array('access'=>'ZR04',),//所有請假列表
-	'Work overtime and leave after the review is cancelled'=>array('access'=>'ZR05',),//取消審核后的加班、請假
-	'Generation ask for leave'=>array('access'=>'ZR06',),//代請假
-	'salary deduction is shown'=>array('access'=>'ZR07',),//工資扣減顯示
-	'All evaluation lists'=>array('access'=>'ZR08',),//所有評估列表
-	'Search all review(local)'=>array('access'=>'ZR09',),//查询所有考核
-	'Search all review(all)'=>array('access'=>'ZR15',),//查询所有考核
-	'Daily incoming and outgoing email'=>array('access'=>'ZR10',),//入职、离职总览电邮
-	'Search all support'=>array('access'=>'ZR11',),//查询所有支援
-	'Back to review'=>array('access'=>'ZR12',),//退回单个考核
-	'Back to leave and work'=>array('access'=>'ZR13',),//退回請假單及加班單
-	'sales group manage'=>array('access'=>'ZR14',),//銷售分組管理
-	'Back to boss review'=>array('access'=>'ZR16',),//退回老總年度考核
-	'delete contract for finish'=>array('access'=>'ZR17',),//刪除已完成的合同簽收
-	'Back to prize'=>array('access'=>'ZR18',),//退回已完成的錦旗
-	'Back to leave office'=>array('access'=>'ZR19',),//退回已離職員工
-	'Show review rankings'=>array('access'=>'ZR20',),//顯示評核得分排名
-	'Show all treaty'=>array('access'=>'ZR21',),//顯示所有提醒小助手
 );
